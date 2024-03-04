d43f4fd3641f4ea976fa7725c2a17bc563f9c2a3
Emilio Font <emifont01@gmail.com>
2024-03-04 15:09:42 -0400

    Create .wezterm.lua
    add .wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+
+config.color_scheme = 'Catppuccin Mocha'
+
+config.font =
+  wezterm.font('JetBrains Mono', { weight = 'Bold'})
+return config
