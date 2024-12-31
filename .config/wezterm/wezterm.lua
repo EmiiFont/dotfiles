f67674a631e0a9566b9c3a1a37968c7f666dc7c9
Emilio Font <emifont01@gmail.com>
2024-12-31 10:46:59 -0400

    Update wezterm.lua

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -13,19 +13,18 @@ local function basename(s)
 	return string.gsub(s, "(.*[/\\])(.*)", "%2")
 end
 
-config.color_scheme = "OneDark (base16)"
+config.color_scheme = "SoftServer"
 
 config.font = wezterm.font("JetBrains Mono")
 config.underline_thickness = "200%"
 config.underline_position = "-3pt"
-config.font_size = 15.0
+config.font_size = 13.0
 config.cursor_blink_ease_in = "Constant"
 config.cursor_blink_ease_out = "Constant"
 config.cursor_blink_rate = 500
 config.default_cursor_style = "BlinkingBlock"
 config.warn_about_missing_glyphs = true
 config.use_fancy_tab_bar = false
-config.window_background_opacity = 0.96
 config.tab_max_width = 16
 config.window_padding = {
 	left = 5,
@@ -35,7 +34,7 @@ config.window_padding = {
 }
 config.initial_cols = 110
 config.initial_rows = 25
-config.window_decorations = "RESIZE"
+config.window_decorations = "TITLE | RESIZE"
 
 wezterm.on("toggle-opacity", function(window, pane)
 	local overrides = window:get_config_overrides() or {}
@@ -51,7 +50,6 @@ end)
 config.switch_to_last_active_tab_when_closing_tab = true
 config.pane_focus_follows_mouse = true
 config.scrollback_lines = 5000
-
 
 keymaps.setup(config)
 
